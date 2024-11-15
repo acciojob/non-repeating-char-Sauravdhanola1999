43126ae57dd239d40c868fdf49ecba902e18668e
Saurav Singh Dhanola <125140898+Sauravdhanola1999@users.noreply.github.com>
2024-11-15 14:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freqObj = {}
+
+	for(let i = 0; i < str.length; i++)
+		{
+			if(!freqObj[str.charAt(i)])
+			{
+				freqObj[str.charAt(i)] = 1
+			}
+			else
+			{
+				freqObj[str.charAt(i)] = freqObj[str.charAt(i)] + 1
+			}
+		}
+
+		for(let char of str){
+		if (charCount[char] === 1) {
+			return char;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
